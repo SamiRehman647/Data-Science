8dc442392c7831ee5357b5c5a41d7e8033f3b9cc
SamiRehman647 <csami647@gmail.com>
2022-01-01 02:02:33 +0500

    Work Complete

---
+++ Assignment 1/Q3.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+today=datetime.today()
+print('Current date and time : ',today)
---
+++ Assignment 1/Q4.py
@@ -0,0 +1,5 @@
+import math
+radius=input('Enter radius')
+r=float(radius)
+area=math.pi*r*r
+print('Area of a circle : ',area)
